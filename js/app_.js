23ff17583709c9a7f0d8cae6fa413b0a02b5cf06
Alexander.l <Alexander.6H9@gmail.com>
2019-02-13 17:44:28 +1000

    Maybe redundant change, witch increases complexity. Client storage changed to avoid initialization of _parsedStorageCookie in save or any other public method to provide correct work of private methods without _parsedStorageCookie been checked. In addition: cookie save-bug fixed.

--- js/app_.js
+++ js/app_.js
@@ -1,4 +1,9 @@
 (function() {
+  // Sometimes (usually when page is opened from HDD) script
+  // doesn't stop on breakpoints. Call to window.prompt is used
+  // to suspend script to get a time to open dev console and
+  // check/set breackpoints.
+  window.prompt('12345');
   const serverStorage = new window.list_app.ServerStorage;
   const clientStorage = new window.list_app.ClientStorage('list_app');
   const storage = new window.list_app.Storage(clientStorage, serverStorage);
